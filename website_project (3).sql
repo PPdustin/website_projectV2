1b91a37af2a43b19552eb885f352d81b0a0f7945
PPdustin <dastennn02@gmail.com>
2023-12-28 14:54:02 +0800

    final project w/ database

--- website_project (3).sql
+++ website_project (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2023 at 05:55 AM
+-- Generation Time: Dec 28, 2023 at 07:52 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -24,6 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `user_type` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`user_name`, `password`, `first_name`, `last_name`, `user_type`) VALUES
+('admin', '12345678', 'admin', 'admin', 'osa'),
+('agredadale', '12345678', 'dale', 'agreda', 'student'),
+('alvaricoish', '12345678', 'ish', 'alvarico', 'faculty'),
+('asd', '12345678', 'asd', 'fda', 'faculty'),
+('carlojose', '12345678', 'Carlo', 'Jose', 'student'),
+('carlosjuan', '12345678', 'Juan', 'Carlos', 'faculty'),
+('diznutz', '12345678', 'diz', 'nutz', 'student'),
+('dummy', '12345678', 'dumy', 'dumy', 'student'),
+('estoresivan', '12345678', 'Ivan', 'Estores', 'student'),
+('lozadajesse', '12345678', 'Jesse', 'Lozada', 'student'),
+('student_1', '12345678', 'student', '1', 'student'),
+('student_one', '12345678', 'student', 'one', 'student'),
+('teacher_1', '12345678', 'teacher', '1', 'faculty'),
+('teacher_one', '12345678', 'apple', 'uy', 'faculty'),
+('watadustin', '12345678', 'Dustin', 'Wata', 'student');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `calendar_event_master`
 --
 
@@ -32,17 +67,20 @@ CREATE TABLE `calendar_event_master` (
   `event_name` varchar(255) DEFAULT NULL,
   `event_start_date` date DEFAULT NULL,
   `event_end_date` date DEFAULT NULL,
-  `is_approved` tinyint(1) NOT NULL,
+  `is_approved` int(1) NOT NULL,
   `venue` varchar(255) NOT NULL,
-  `submitted_by` int(11) NOT NULL
+  `submitted_by` varchar(255) NOT NULL,
+  `parents_permit` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `calendar_event_master`
 --
 
-INSERT INTO `calendar_event_master` (`event_id`, `event_name`, `event_start_date`, `event_end_date`, `is_approved`, `venue`, `submitted_by`) VALUES
-(4, 'Party', '2023-12-03', '2023-12-03', 1, 'CA1', 1);
+INSERT INTO `calendar_event_master` (`event_id`, `event_name`, `event_start_date`, `event_end_date`, `is_approved`, `venue`, `submitted_by`, `parents_permit`) VALUES
+(18, 'TEST', '2023-12-26', '2023-12-26', 0, 'DAVAO', 'dumy dumy', NULL),
+(19, 'dummy', '2023-12-20', '2023-12-21', 2, 'DAVAO', 'student 1', 'submitted'),
+(20, 'new year', '2023-12-31', '2024-01-02', 2, 'ca1', 'student one', 'submitted');
 
 -- --------------------------------------------------------
 
@@ -54,7 +92,7 @@ CREATE TABLE `club` (
   `club_id` int(10) NOT NULL,
   `club_name` varchar(255) NOT NULL,
   `club_description` varchar(255) NOT NULL,
-  `facilitator` int(11) NOT NULL
+  `facilitator` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -62,7 +100,12 @@ CREATE TABLE `club` (
 --
 
 INSERT INTO `club` (`club_id`, `club_name`, `club_description`, `facilitator`) VALUES
-(1, 'Computer Club', 'Computer Club', 1);
+(1, 'Computer Club', 'Computer Club', 1),
+(2, 'MAAS', 'MAAS', 1),
+(3, 'None', 'None', NULL),
+(7, 'PSITS', 'PSITS', 2),
+(9, 'DUMMY', 'DUMMY', 4),
+(10, 'club_one', 'ads', 6);
 
 -- --------------------------------------------------------
 
@@ -113,7 +156,35 @@ CREATE TABLE `faculty` (
 --
 
 INSERT INTO `faculty` (`faculty_id`, `first_name`, `last_name`) VALUES
-(1, 'Ish', 'Alvarico');
+(1, 'Ish', 'Alvarico'),
+(2, 'Juan', 'Carlos'),
+(4, 'teacher', '1'),
+(5, 'asd', 'fda'),
+(6, 'apple', 'uy'),
+(7, 'apple', 'uy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permit`
+--
+
+CREATE TABLE `permit` (
+  `permit_id` int(11) NOT NULL,
+  `event_id` int(10) NOT NULL,
+  `submitted_by` varchar(255) NOT NULL,
+  `file_title` varchar(255) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `club` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `permit`
+--
+
+INSERT INTO `permit` (`permit_id`, `event_id`, `submitted_by`, `file_title`, `file_path`, `club`) VALUES
+(7, 19, 'student 1', 'interview (1).docx', 'C:/xampp/htdocs/website_projectV2/permits/6580574619210_interview (1).docx', 'DUMMY'),
+(8, 20, 'student one', 'interview (1).docx', 'C:/xampp/htdocs/website_projectV2/permits/658d12c0e24ce_interview (1).docx', 'club_one');
 
 -- --------------------------------------------------------
 
@@ -123,16 +194,21 @@ INSERT INTO `faculty` (`faculty_id`, `first_name`, `last_name`) VALUES
 
 CREATE TABLE `position` (
   `position_id` int(10) NOT NULL,
-  `position_name` varchar(255) NOT NULL,
-  `position_description` varchar(255) NOT NULL
+  `position_name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `position`
 --
 
-INSERT INTO `position` (`position_id`, `position_name`, `position_description`) VALUES
-(1, 'President', 'President');
+INSERT INTO `position` (`position_id`, `position_name`) VALUES
+(1, 'President'),
+(2, 'Vice-President'),
+(3, 'Secretary'),
+(4, 'Treasurer'),
+(5, 'PIO'),
+(6, 'Auditor'),
+(7, 'None');
 
 -- --------------------------------------------------------
 
@@ -145,7 +221,7 @@ CREATE TABLE `student` (
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `course` int(10) NOT NULL,
-  `club` int(10) NOT NULL,
+  `club` int(10) DEFAULT NULL,
   `position` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -154,18 +230,31 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`student_id`, `first_name`, `last_name`, `course`, `club`, `position`) VALUES
-(1, 'Dustin', 'Wata', 1, 1, 1);
+(1, 'Dustin', 'Wata', 1, 2, 2),
+(2, 'Dale', 'Agreda', 1, 1, 2),
+(3, 'Jesse', 'Lozada', 1, 2, 1),
+(4, 'Ivan', 'Estores', 1, 2, 6),
+(5, 'diz', 'nutz', 1, 1, 4),
+(6, 'Carlo', 'Jose', 1, 7, 7),
+(7, 'dumy', 'dumy', 1, 3, 7),
+(8, 'student', '1', 1, 9, 1),
+(9, 'student', 'one', 1, 10, 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`user_name`);
+
+--
 -- Indexes for table `calendar_event_master`
 --
 ALTER TABLE `calendar_event_master`
-  ADD PRIMARY KEY (`event_id`),
-  ADD KEY `submitted_by` (`submitted_by`);
+  ADD PRIMARY KEY (`event_id`);
 
 --
 -- Indexes for table `club`
@@ -193,6 +282,12 @@ ALTER TABLE `faculty`
   ADD PRIMARY KEY (`faculty_id`);
 
 --
+-- Indexes for table `permit`
+--
+ALTER TABLE `permit`
+  ADD PRIMARY KEY (`permit_id`);
+
+--
 -- Indexes for table `position`
 --
 ALTER TABLE `position`
@@ -204,7 +299,6 @@ ALTER TABLE `position`
 ALTER TABLE `student`
   ADD PRIMARY KEY (`student_id`),
   ADD KEY `course` (`course`),
-  ADD KEY `position` (`position`),
   ADD KEY `club` (`club`);
 
 --
@@ -215,13 +309,13 @@ ALTER TABLE `student`
 -- AUTO_INCREMENT for table `calendar_event_master`
 --
 ALTER TABLE `calendar_event_master`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `club`
 --
 ALTER TABLE `club`
-  MODIFY `club_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `club_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `course`
@@ -239,29 +333,29 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT for table `faculty`
 --
 ALTER TABLE `faculty`
-  MODIFY `faculty_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `faculty_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `permit`
+--
+ALTER TABLE `permit`
+  MODIFY `permit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `position`
 --
 ALTER TABLE `position`
-  MODIFY `position_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `position_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `student_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `student_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `calendar_event_master`
---
-ALTER TABLE `calendar_event_master`
-  ADD CONSTRAINT `calendar_event_master_ibfk_1` FOREIGN KEY (`submitted_by`) REFERENCES `student` (`student_id`);
 
 --
 -- Constraints for table `club`
